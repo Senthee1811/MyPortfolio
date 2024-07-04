9888161fba2fba2fddcfd8dda4331b6ae34de70c
Senthee1811 <esentheepan@gmail.com>
2024-07-04 20:09:52 +0530

    Improvised

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const toTop = document.querySelector(".back-top"); 
 
-addEventListener("scroll",() => {
-    if(pageXOffset > 75){
-        toTop.classList.add("active")
-    }else{
-        toTop.classList.remove("active")
-    }
-})
+document.addEventListener("DOMContentLoaded", () => {
+    const toTop = document.querySelector(".back-to-top");
+
+    window.addEventListener("scroll", () => {
+        if (window.pageYOffset > 75) {
+            toTop.classList.add("active");
+        } else {
+            toTop.classList.remove("active");
+        }
+    });
+
+    toTop.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
